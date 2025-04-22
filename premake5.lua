90e0d73facdb7bdf4e815117074ea4a429a235c9
jonas-bechtel <jonas.bechtel@t-online.de>
2025-04-22 15:07:03 +0200

    added premake file

---
+++ premake5.lua
@@ -0,0 +1,23 @@
+project "Betacool"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "off"
+
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files 
+    { 
+        "src/**.cc", 
+        "src/**.h"
+    }
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
+
